aea64b381fa20405335e3e689b08fd7c2cd0cb61
Sascha Wander <sascha.wander@gmail.com>
2021-07-26 00:20:41 +0200

    stdout-log

--- lib/stdout-log.sh
+++ lib/stdout-log.sh
@@ -2,24 +2,24 @@
 
 msg () {
     # bold/yellow
-    printf "\033[1m\033[93m=>\033[m %s\n" "$*"
+    printf "\033[1m\033[93m=>\033[m $@\n"
 }
 
 info () {
     # bold/yellow
-    printf "\033[1m\033[93mINFO:\033[m %s\n" "$*"
+    printf "\033[1m\033[93mINFO:\033[m $@\n"
 }
 
-warning () {
+warn () {
     # bold/orange
-    printf "\033[1m\033[94mWARNING:\033[m %s\n" "$*"
+    printf "\033[1m\033[94mWARNING:\033[m $@\n"
 }
 
 error () {
     # bold/red
-    printf "\033[1m\033[31mERROR:\033[m %s\n" "$*"
+    printf "\033[1m\033[31mERROR:\033[m $@\n"
 }
 
 die () {
-    local ret="$?"; error "$*"; exit "$ret"
+    local ret="$?"; error "$@"; exit "$ret"
 }
