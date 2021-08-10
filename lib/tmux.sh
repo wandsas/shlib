c75245003313189c6ff208a96fdf0b19d0d6f787
Sascha Wander <sascha.wander@gmail.com>
2021-08-10 18:41:03 +0200

    enhancingmytmux

--- lib/tmux.sh
+++ lib/tmux.sh
@@ -1,18 +1,18 @@
 . ~/lib/stdout-log.sh
 
 # Get current tmux session.
-tmux-current-session () {
+tmux_current_session () {
     if [ "$TMUX" ]; then
         tmux display-message -pF '#{client_session}'
     fi
 }
 
 # Do we have a running system?
-tmux-has-session () {
+tmux_has_session () {
     tmux has-session -t "$1" 2>/dev/null
 }
 
-tmux-check-session-var () {
+tmux_check_session_var () {
     if [ -z "$tmux_session" ]; then
         echo >&2 "caller must define \$session"
         exit 1
@@ -20,16 +20,16 @@ tmux-check-session-var () {
 }
 
 # Create new tmux session, or return existing session if it exists.
-tmux-new-session () {
+tmux_new_session () {
     if [ -z "$tmux_session" ]; then
         tmux_session="$1"
     fi
 
-    tmux-check-session-var
+    tmux_check_session_var
 
-    if tmux-has-session "$tmux_session"; then
+    if tmux_has_session "$tmux_session"; then
         echo "$tmux_session session already exists!  Attaching ..."
-        tmux-attach
+        tmux_attach
         exit
     fi
 
@@ -39,32 +39,32 @@ tmux-new-session () {
     fi
 
     # The -d is necessary to allow the script to proceed
-    tmux new-session -d -s "$tmux_session"
+    tmux_new_session -d -s "$tmux_session"
 }
 
 # Create new tmux window.
-tmux-new-window () {
-    tmux-check-session-var
+tmux_new_window () {
+    tmux_check_session_var
     tmux new-window -t "$tmux_session" "$@"
 }
 
 # Attach tmux session.
-tmux-attach () {
+tmux_attach () {
     tmux attach-session -t "$tmux_session"
 }
 
-tmux-detach () {
+tmux_detach () {
     tmux detach-client
 }
 
 # Kill current tmux session.
-tmux-kill-session () {
+tmux_kill_session () {
     if [ -n "$TMUX" ]; then
         tmux kill-session -t $(tmux display-message -pF '#{client_session}')
     fi
 }
 
 # Find all tmux sessions and kill them.
-tmux-kill-all-sessions () {
+tmux_kill_all_sessions () {
     tmux list-sessions | awk -F: '{print $1}' | xargs -n 1 tmux kill-session -t
 }
